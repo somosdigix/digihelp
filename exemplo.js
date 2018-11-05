8fa0d4bffb473c983d9f657e70526a8ac4d12892
diegopaniago <diegopaniago1@gmail.com>
2018-11-05 15:54:25 -0300

    Removendo console log perdido

--- exemplo.js
+++ exemplo.js
@@ -1,7 +1,6 @@
 import DigiHelp from './dist/digihelp';
 import listaDeAjuda from './listaDeAjuda.json';
 
-console.log(DigiHelp);
 let digiHelp = new DigiHelp(listaDeAjuda);
 
 window.onhashchange = () => {
