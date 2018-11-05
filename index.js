ac1ba86302c2d71c309ca2258a24ec1b21e9f7dd
diegopaniago <diegopaniago1@gmail.com>
2018-11-05 13:18:23 -0300

    Corrigindo o main di projeto

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import DigiHelp from './app/digihelp';
 import listaDeAjuda from './listaDeAjuda.json';
 
-console.log(DigiHelp);
-
 let digiHelp = new DigiHelp(listaDeAjuda);
 
 window.onhashchange = () => {
